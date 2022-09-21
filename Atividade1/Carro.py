6eb61564240bc43ca5bcca0b98a2fcef3e5746de
Alisson Lang Brustolin <114074403+AlissonLangBrustolin@users.noreply.github.com>
2022-09-21 09:45:15 -0300

    Definição do Carro

---
+++ Atividade1/Carro.py
@@ -0,0 +1,30 @@
+class Carro:
+
+    def __init__(self, cor, ligado=False, velocidade=0, espelhamento=False, pelicula=False):
+        self.cor = cor
+        self.ligado = ligado
+        self.velocidade = velocidade
+        self.espelhamento = espelhamento
+        self.pelicula = pelicula
+
+    def ligar(self):
+        if not self.ligado:
+            self.ligado = True
+        else:
+            self.ligado = False
+
+    def acelerar(self, aceleracao):
+        if self.ligado:
+            self.velocidade += aceleracao
+
+    def modificar(self, nova_cor, novo_espelhamento, nova_pelicula):
+        self.cor = nova_cor
+        self.espelhamento = novo_espelhamento
+        self.pelicula = nova_pelicula
+
+c1 = Carro('Amarelo')
+print(vars(c1))
+c1.modificar('Preto', True, True)
+c1.ligar()
+c1.acelerar(50)
+print(vars(c1))
